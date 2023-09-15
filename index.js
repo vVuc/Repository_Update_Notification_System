f7bc7dce74ef3977bd35d150587c7bf0b46d1439
Thiago Menezes da Silva <tmenezessilva34@gmail.com>
2023-09-15 03:09:36 -0300

    wip

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const funct = async (owner, repo) => {
     try {
         const {data} = await axios.get(`https://api.github.com/repos/${owner}/${repo}/commits`)
         const {author, message} = data[0].commit
-        const obj = {author: author.name, date: author.date, message}
         const MinhaApi = await axios.post(`http://localhost:6565`, {author: author.name, date: author.date, message})
 
         console.log(MinhaApi.data)
